c3b510896be791d33be97aa45f861aecdb29d8f4
Nandan-18 <nandanramesh123@gmail.com>
2024-12-04 16:12:12 -0700

    oopsies

--- scripts/run.bat
+++ scripts/run.bat
@@ -2,7 +2,7 @@ if not exist build mkdir build
 cd build
 cmake ../ -G "Visual Studio 17 2022"
 msbuild .\BasicSc2Bot.sln
+cd ..
 set /p race="Enter race (terran, protoss, zerg): "
 set /p difficulty="Enter difficulty (VeryEasy, Easy, Medium, Hard, VeryHard): "
-bin\BasicSc2Bot.exe -c -a %race% -d %difficulty% -m CactusValleyLE.SC2Map
-cd ..
+.\build\bin\BasicSc2Bot.exe -c -a %race% -d %difficulty% -m CactusValleyLE.SC2Map
